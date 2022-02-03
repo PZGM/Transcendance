23f442ee50f9de5456f3a74564c3615149652d7b
A-freire <bexas94@gmail.com>
2022-02-03 11:48:02 +0000

    Fin transition de header en class

--- frontend/src/asset/Header.tsx
+++ frontend/src/asset/Header.tsx
@@ -10,6 +10,8 @@ interface HeaderProps {};
 interface HeaderState {
 	avatar?: string,
 	login?: string,
+	anchorElUser?: null | HTMLElement,
+	anchorElNav?: null | HTMLElement,
   }
 
 
@@ -18,7 +20,7 @@ interface HeaderState {
 export class Header extends Component<HeaderProps, HeaderState>{
 	constructor(props: HeaderProps) {
 		super(props);
-		this.state = {avatar: undefined, login: undefined}
+		this.state = {avatar: undefined, login: undefined, anchorElUser: null, anchorElNav: null}
 		this.updateHeaderState = this.updateHeaderState.bind(this);
 	}
 
@@ -45,101 +47,56 @@ export class Header extends Component<HeaderProps, HeaderState>{
 		this.fetchUser();
 	}
 
-
-
-	// const [login, setLogin] = useState<string | undefined>(undefined);
-	// const [avatar, setAvatar] = useState<string | undefined>(undefined);
-	// const [user, setUser] = useState(null);
-
-
-	// const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
-	// const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   
-	// const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-	//   setAnchorElNav(event.currentTarget);
-	// };
-	// const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-	//   setAnchorElUser(event.currentTarget);
-	// };
+	handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+	  this.setState({anchorElNav: event.currentTarget});
+	};
+
+	handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+	  this.setState({anchorElUser: event.currentTarget});
+	};
   
-	// const handleCloseNavMenu = () => {
-	//   setAnchorElNav(null);
-	// };
+	handleCloseNavMenu = () => {
+	  this.setState({anchorElNav: null});
+	};
 	
-	// const handleCloseUserMenu = () => {
-	// 	setAnchorElUser(null);
-	//   };
-
-
-	// useEffect(() => {
-	// 	async function fetchUser() {
-
-	// 		const resp = await UserAPI.getUser();
-	// 		// setLogin(resp.login);
-	// 		this.state.avatar = resp.img_url;
-	// 	}
-	// 	fetchUser();
-	// }, [this.state.avatar]);
-
-	//   console.log("yo");
-	//   console.log(this.state.avatar);
-	//   console.log("merde");
-
-
-
-
-
-	// const updateHeaderStates = function (image: string) {
-	// 	this.setState({
-	// 		setAvatar(image)
-	// 	});
-	// }
-
-
-
-
-
+	handleCloseUserMenu = () => {
+		this.setState({anchorElUser: null});
+	};
 
 
 	render () {
-		console.log(`===RENDER=== login = ${this.state.login}`)
-	return (
-		<div>
-			<Profil_Card login={this.state.login} updateHeaderState={this.updateHeaderState}></Profil_Card>
-			<Typography>{this.state.avatar}</Typography>
-		</div>
-// 	<AppBar position="static">
-// 	<Box>
-// 		<Toolbar>
-// 			<Box sx={{ flexGrow: 1, display: { xs: 'flex'} }}>
-// 				<IconButton
-// 				size="large"
-// 				aria-label="account of current user"
-// 				aria-controls="menu-appbar"
-// 				aria-haspopup="true"
-// 				onClick={handleOpenNavMenu}
-// 				color="inherit"
-// 				>
-// 					<MenuIcon />
-// 				</IconButton>
-// 			</Box>
-
-// 			<Box sx={{ flexGrow: 0 }}>
-// 				<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-// 					{/* <Avatar alt="Semy Sharp" src='https://upload.wikimedia.org/wikipedia/commons/f/f1/Charles_Darwin_portrait.jpg' /> */}
-// 					<Avatar alt="Semy Sharp" src={this.state.avatar} />
-// 				</IconButton>
-// 				<Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={anchorElUser} anchorOrigin={{vertical: 'top',horizontal: 'left'}} keepMounted transformOrigin={{vertical: 'top',horizontal: 'right',}} open={Boolean(anchorElUser)} onClose={handleCloseUserMenu} >
-// 					{
-// 						<Box>
-// 							<Profil_Card Login={'billy'}/>
-// 						</Box>
-// 					}
-// 				</Menu>
-// 			</Box>
-// 		</Toolbar>
-// 	</Box>
-// </AppBar>
-	);
-				}
+		return (
+			<AppBar position="static">
+				<Box>
+					<Toolbar>
+						<Box sx={{ flexGrow: 1, display: { xs: 'flex'} }}>
+							<IconButton 
+							size="large"
+							aria-label="account of current user"
+							aria-controls="menu-appbar"
+							aria-haspopup="true"
+							onClick={this.handleOpenNavMenu}
+							color="inherit"
+							>
+								<MenuIcon />
+							</IconButton> 
+						</Box> 
+						<Box sx={{ flexGrow: 0 }}>
+							<IconButton onClick={this.handleOpenUserMenu} sx={{ p: 0 }}>
+								<Avatar alt="Semy Sharp" src={this.state.avatar} />
+							</IconButton>
+							<Menu sx={{ mt: '45px' }} id="menu-appbar" anchorEl={this.state.anchorElUser} anchorOrigin={{vertical: 'top',horizontal: 'left'}} keepMounted transformOrigin={{vertical: 'top',horizontal: 'right',}} open={Boolean(this.state.anchorElUser)} onClose={this.handleCloseUserMenu} >  
+								{ 
+									<Box>
+										<Profil_Card login={this.state.login} updateHeaderState={this.updateHeaderState}></Profil_Card>
+									</Box>
+								}
+							</Menu> 
+						</Box> 
+					</Toolbar> 
+				</Box>
+			</AppBar>
+		);
+	}
 }
--- frontend/test.tsx
+++ frontend/test.tsx
@@ -1,122 +1,140 @@
 import * as React from 'react';
-import { styled } from '@mui/material/styles';
+import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-import ButtonBase from '@mui/material/ButtonBase';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
+import Menu from '@mui/material/Menu';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Tooltip from '@mui/material/Tooltip';
+import MenuItem from '@mui/material/MenuItem';
 
-const images = [
-  {
-    url: '/static/images/buttons/breakfast.jpg',
-    title: 'Breakfast',
-    width: '40%',
-  },
-  {
-    url: '/static/images/buttons/burgers.jpg',
-    title: 'Burgers',
-    width: '30%',
-  },
-  {
-    url: '/static/images/buttons/camera.jpg',
-    title: 'Camera',
-    width: '30%',
-  },
-];
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
-const ImageButton = styled(ButtonBase)(({ theme }) => ({
-  position: 'relative',
-  height: 200,
-  [theme.breakpoints.down('sm')]: {
-    width: '100% !important', // Overrides inline-style
-    height: 100,
-  },
-  '&:hover, &.Mui-focusVisible': {
-    zIndex: 1,
-    '& .MuiImageBackdrop-root': {
-      opacity: 0.15,
-    },
-    '& .MuiImageMarked-root': {
-      opacity: 0,
-    },
-    '& .MuiTypography-root': {
-      border: '4px solid currentColor',
-    },
-  },
-}));
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
-const ImageSrc = styled('span')({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center 40%',
-});
+  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-const Image = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.common.white,
-}));
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
 
-const ImageBackdrop = styled('span')(({ theme }) => ({
-  position: 'absolute',
-  left: 0,
-  right: 0,
-  top: 0,
-  bottom: 0,
-  backgroundColor: theme.palette.common.black,
-  opacity: 0.4,
-  transition: theme.transitions.create('opacity'),
-}));
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-const ImageMarked = styled('span')(({ theme }) => ({
-  height: 3,
-  width: 18,
-  backgroundColor: theme.palette.common.white,
-  position: 'absolute',
-  bottom: -2,
-  left: 'calc(50% - 9px)',
-  transition: theme.transitions.create('opacity'),
-}));
-
-export default function ButtonBases() {
   return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', minWidth: 300, width: '100%' }}>
-      {images.map((image) => (
-        <ImageButton
-          focusRipple
-          key={image.title}
-          style={{
-            width: image.width,
-          }}
-        >
-          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
-          <ImageBackdrop className="MuiImageBackdrop-root" />
-          <Image>
-            <Typography
-              component="span"
-              variant="subtitle1"
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
+          >
+            LOGO
+          </Typography>
+
+          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
               color="inherit"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'left',
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
               sx={{
-                position: 'relative',
-                p: 4,
-                pt: 2,
-                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
+                display: { xs: 'block', md: 'none' },
               }}
             >
-              {image.title}
-              <ImageMarked className="MuiImageMarked-root" />
-            </Typography>
-          </Image>
-        </ImageButton>
-      ))}
-    </Box>
+              {pages.map((page) => (
+                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                  <Typography textAlign="center">{page}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
+          >
+            LOGO
+          </Typography>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                onClick={handleCloseNavMenu}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: '45px' }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: 'top',
+                horizontal: 'right',
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {settings.map((setting) => (
+                <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <Typography textAlign="center">{setting}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
-}
+};
+export default ResponsiveAppBar;
