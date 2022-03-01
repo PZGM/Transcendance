0bf86d0d18f8ff47f4e16c197c4186e39da7e36d
pzgm <pizza@gami.pz>
2022-03-01 16:10:02 +0100

    script working lets do the chat

--- exec.sh
+++ exec.sh
@@ -2,7 +2,7 @@
 
 option=""
 
-if [ $0 = "-a" ] || [[ $# = 1  &&  $1 = "-a" ]] || [[ $# = 2  &&  $2 = "-a" ]] 
+if [[ "$#"  -ge  "1"  &&   $1 = "-a" ]] || [[ "$#"  -ge "2"   &&   $2 = "-a" ]] || [[ "$#"  -ge "3"  &&  $3 = "-a" ]] 
 then
 	export front=4000
 	export http=4001
@@ -11,7 +11,7 @@ then
 	export adminer=4888
 	export client_id=86fc50a7e3722acbd29db85054adff0ea138a6374c2826daff10307850cd8914
 	export client_secret=8f76f1fdbe01fab8ab9e950c0f480c8330c7732d47250f62c4c1ffbb3491a5bc
-elif [ $0 = "-b" ] || [[ $# = 1  &&  $1 = "-b" ]] || [[ $# = 2  &&  $2 = "-b" ]] 
+elif [[ "$#"  -ge "1"  &&   $1 = "-b" ]] || [[ "$#"  -ge  "2"   &&  $2 = "-b" ]] || [[ "$#"  -ge  "3"  &&  $3 = "-b" ]]
 then
 	export front=3000
 	export http=3001
@@ -20,7 +20,7 @@ then
 	export adminer=3888
 	export client_id=f0a2ad126cf0cf8efe2ef0f4112fa0b3bba245e61fe0d6695680edff7810464d
 	export client_secret=bced843a0688e6965579e4722a5e0ebba759f935be48e2a1d2f9910be108bf82
-elif [ $0 = "-f" ] || [[ $# = 1  &&  $1 = "-f" ]] || [[ $# = 2  &&  $2 = "-f" ]] 
+elif [[ "$#"  -ge  "1"  &&  $1 = "-f" ]] || [[ "$#"  -ge  "2"  &&  $2 = "-f" ]] || [[ "$#"  -ge  "3"  &&  $3 = "-f" ]]
 then
 	export front=5000
 	export http=5001
@@ -29,7 +29,7 @@ then
 	export adminer=5888
 	export client_id=8c41ad58501e58d3f2979a7d68075623ce3147dd306b6f5f340ec836323cbfd6
 	export client_secret=a26f458f3c4b35522241a132a4479e2d8514f8607a158183ea5bb87e6123169e
-elif [ $0 = "-s" ] || [[ $# = 1  &&  $1 = "-s" ]] || [[ $# = 2  &&  $2 = "-s" ]] 
+elif [[ "$#"  -ge "1"  &&  $1 = "-s" ]] || [[ "$#"  -ge "2"  &&  $2 = "-s" ]] || [[ "$#"  -ge "3"  &&  $3 = "-s" ]]
 then
 	export front=6000
 	export http=6001
@@ -39,16 +39,19 @@ then
 	export client_id=3cfe88d0df43c58af6ec6bcdc82c51db030f67ceb8901022cd40a884ab693849
 	export client_secret=3a09b304e77486300b12815bbc50e327672e95ccf9b193090f919b3d89f5889e
 else
-	echo " You must select a pzgm member with an argument (-a for adrien -b for billy ect)"
+	echo " You must select a pzgm member with an argument (-a for adrien -b for billy ect...)"
 	exit 0
 fi
 
-if [ $0 = "-d" ] || [[ $# = 1  &&  $1 = "-d" ]] || [[ $# = 2  &&  $2 = "-d" ]]
+if [[ "$#"  -ge "1"  &&   $1 = "-d" ]] || [[ "$#"  -ge "2"   &&   $2 = "-d" ]] || [[ "$#"  -ge "3"  &&  $3 = "-d" ]] 
 then
 	option="-d"
 fi
 
+if [[ "$#"  -ge "1"  &&   $1 = "-r" ]] || [[ "$#"  -ge "2"   &&   $2 = "-r" ]] || [[ "$#"  -ge "3"  &&  $3 = "-r" ]] 
+then
 	docker system prune -af
+fi
 
 docker build -t back "./backend" 
 docker build -t front "./frontend"
