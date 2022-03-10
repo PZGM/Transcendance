7159bd1a88488c8831cb1da2d2a368a29a364947
fabri <fa@bri.it>
2022-03-10 15:38:49 +0100

    Friends fmanetti

--- frontend/src/menu/MenuButton.tsx
+++ frontend/src/menu/MenuButton.tsx
@@ -1,3 +1,4 @@
+import { Grid } from '@mui/material';
 import { Avatar, Box, Button, Stack, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 import CloseIcon from '@mui/icons-material/Close';
@@ -6,11 +7,7 @@ import CloseIcon from '@mui/icons-material/Close';
 export default function MenuButton() {
 
 	return (
-        <Stack direction="column" justifyContent="space-evenly" alignItems="center" spacing={2}>
-            <Stack direction="row" justifyContent="flex-end" alignItems="flex-start" sx={{width: "100%"}}>
-                <Button component={Link} to={process.env.REACT_APP_HOME as string} sx={{backgroundColor: 'red'}}><CloseIcon/></Button>
-            </Stack>
-            <Avatar variant='circular' alt="Semy Sharp" src="/static/images/avatar/1.jpg" sx={{ }}/>
+        <Grid container direction="column" justifyContent="center" alignItems="center">
             {/* <Typography>{this.state.login}</Typography> */}
             <Typography>AFREIRE-</Typography>
             <Button component={Link} to={process.env.REACT_APP_PROFILE as string}>Profile</Button>
@@ -19,6 +16,6 @@ export default function MenuButton() {
             <Button component={Link} to={process.env.REACT_APP_HISTORY as string}>Match History</Button>
             <Button component={Link} to={process.env.REACT_APP_ACHIEVEMENT as string}>Achievement</Button>
             <Box></Box>
-        </Stack>
+        </Grid>
 );
 }
