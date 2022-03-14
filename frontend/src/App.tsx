6bd396d047bfa5bccf9fab937483c74a194fc9b6
fabri <fa@bri.it>
2022-03-14 18:38:24 +0100

    Login page and 42connect

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
-import { Helmet } from 'react-helmet';
 import { Stack } from "@mui/material";
+import { sizing } from '@mui/system';
 
 
 
@@ -10,40 +10,43 @@ function App() {
 	
 	return (
 
-		<div className="App">
-			<Helmet>
-				<style>{'body { background-color: black; }'}</style>
-			</Helmet>
-			
+		<div
+			style={{
+				backgroundColor: 'black',
+				height: '100vh'
+				}}
+	  	>
+
 			<Stack
 				direction="column"
 				justifyContent="space-between"
 				alignItems="center"
-				className="test"
-				>
-				<img src={require('./asset/images/ft_transcendance.png')} className='fit' alt=""/>
+				className='stack'
+			>
+					<img	src={require('./asset/images/ft_transcendance.png')}
+							style={{width: '100%'}}
+					/>
 
-            	<img
-                src={require('./asset/images/42.gif')}
-                onClick={() => window.open(process.env.REACT_APP_URL_AUTH)}
-				className='fit'
-				alt=""/>
+				<img
+				src={require('./asset/images/42connect.png')}
+				onClick={() => window.open(process.env.REACT_APP_URL_AUTH, "_self")}
+				style={{width: '20%'}}
+				/>
 
 				<div>
 					<img
 						src={require('./asset/images/afreire_braimbau.png')}
-						className='fit1'
-						alt=""/>
+						style={{width: '50%'}}
+						/>
 					<img
 						src={require('./asset/images/fmanetti_selgrabl.png')}
-						className='fit2'
-						alt=""/>
+						style={{width: '50%'}}
+						/>
 				</div>
+
 			</Stack>
-			
-			
-			
-			</div>
+
+		</div>
 	);
 }
 
