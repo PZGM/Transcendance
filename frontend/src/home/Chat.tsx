c2be51f27cc095df398e2b6c650182b5f847d9a6
A-freire <bexas94@gmail.com>
2022-03-14 10:56:25 +0100

    fenetre de chat fini il faut juste tout fire fonctionnel

--- frontend/src/home/Chat.tsx
+++ frontend/src/home/Chat.tsx
@@ -1,6 +1,7 @@
-import { Box, Button, ButtonBase, Stack, Typography } from "@mui/material";
+import { Box, Button, ButtonBase, Grid, IconButton, Input, InputBase, Stack, TextField, Typography } from "@mui/material";
 import { Component} from "react";
-
+import { Selecter } from "./gestion_chat/Selecter";
+import SendIcon from '@mui/icons-material/Send';
 
 
 interface ChatProps {
@@ -21,18 +22,23 @@ export class Chat extends Component<ChatProps> {
 	render () {
 		return (
             <>
-				<Box width="100%" height="50px" sx={{backgroundColor: "blue"}}>
-					<Stack direction="row" justifyContent="center" alignItems="center" spacing={1}>
-						<ButtonBase sx={{backgroundColor: "red", height: 10, width: 10}}>i</ButtonBase>
-						<ButtonBase sx={{backgroundColor: "red",}}>42</ButtonBase>
+				<Selecter></Selecter>
+				<Box height="1500px">
+					<Typography>{`Yo je suis le Chat`}</Typography>
+					<ButtonBase onClick={ () => {this.props.updateDisplay(1, 0);}}> Go to profile 0</ButtonBase>
+					<ButtonBase onClick={ () => {this.props.updateDisplay(1, 1);}}> Go to profile 1</ButtonBase>
+					<ButtonBase onClick={ () => {this.props.updateDisplay(1, 2);}}> Go to profile 2</ButtonBase>
+					<ButtonBase onClick={ () => {this.props.updateDisplay(1, 3);}}> Go to profile 3</ButtonBase>
+				</Box>
+				
+				<Box height="60px" sx={{backgroundColor: "red"}}>
+					<Stack direction="row" spacing={2}>
+						<InputBase inputProps={{style: { textAlign: 'center' }}} placeholder="Send Message" sx={{marginLeft: "5px", width: "80%", height: "60px" }}/>
+						<IconButton>
+							<SendIcon/>
+						</IconButton>
 					</Stack>
 				</Box>
-				<Typography>{`Yo je suis le Chat`}</Typography>
-                <ButtonBase onClick={ () => {this.props.updateDisplay(1, 0);}}> Go to profile 0</ButtonBase>
-				<ButtonBase onClick={ () => {this.props.updateDisplay(1, 1);}}> Go to profile 1</ButtonBase>
-                <ButtonBase onClick={ () => {this.props.updateDisplay(1, 2);}}> Go to profile 2</ButtonBase>
-                <ButtonBase onClick={ () => {this.props.updateDisplay(1, 3);}}> Go to profile 3</ButtonBase>
-
 			</>
 		)
 	}
