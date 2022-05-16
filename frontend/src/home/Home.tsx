337789c58bd8421827734f7db0ad6a7615b02b36
pzgm <pizza@gami.pz>
2022-05-16 15:41:50 +0200

    merge front

--- frontend/src/home/Home.tsx
+++ frontend/src/home/Home.tsx
@@ -1,20 +1,10 @@
-import { Stack } from "@mui/material";
 import React, { Component } from "react";
 import '../App.css';
 import '../style/style.css'
 import '../style/home.css'
 import { UserAPI } from "../api/Users.api";
-import { GameAPI } from "../api/Game.api";
-import SendIcon from '@mui/icons-material/Send';
-import { Link, Outlet, useParams } from "react-router-dom";
-import { MiniStatus } from "../asset/MiniStatus";
-import { height } from "@mui/system";
-import { MyInfos } from "./ChatPannel//header/MyInfos";
 import { ChatAPI } from "../api/Chat.api";
-import { Selecter } from './ChatPannel/header/Selecter'
-import { PrivateGuard } from "../components/PrivateGuard";
 import { Game } from "./Game/Game";
-import { UserInfo } from "./ChatPannel/UserInfo";
 
 
 interface HomeProps {
